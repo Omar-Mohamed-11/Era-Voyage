c4df514fa98e96421a0a33a537fe0e0c01fbbe4c
Omar Mohamed <omarmohamed.official19@gmail.com>
2026-05-10 02:33:15 +0300

    Edit the comments

--- Pharaohs-Page/script.js
+++ Pharaohs-Page/script.js
@@ -1,26 +1,4 @@
-document.querySelector(".card-1").onclick = () => {
-    document.getElementById("popup1").style.display = "flex";
-};
-
-document.querySelector(".card-2").onclick = () => {
-    document.getElementById("popup2").style.display = "flex";
-};
-
-document.querySelector(".card-3").onclick = () => {
-    document.getElementById("popup3").style.display = "flex";
-};
-
-document.querySelector(".card-4").onclick = () => {
-    document.getElementById("popup4").style.display = "flex";
-};
-
-document.querySelectorAll(".close").forEach(btn => {
-    btn.onclick = () => {
-        btn.closest(".popup").style.display = "none";
-    };
-});
-
-// The Story Reader
+// The Story Reader (In section-1)
 function toggleAudio() {
     const icon = document.querySelector(".ai-reader");
     const audio = document.getElementById("story-audio");
@@ -40,6 +18,30 @@ function toggleAudio() {
         icon.textContent = "🕪";
     }
 }
+
+// The cards on section-3 (Show the hiden card)
+document.querySelector(".card-1").onclick = () => {
+    document.getElementById("popup1").style.display = "flex";
+};
+
+document.querySelector(".card-2").onclick = () => {
+    document.getElementById("popup2").style.display = "flex";
+};
+
+document.querySelector(".card-3").onclick = () => {
+    document.getElementById("popup3").style.display = "flex";
+};
+
+document.querySelector(".card-4").onclick = () => {
+    document.getElementById("popup4").style.display = "flex";
+};
+// When click on close make it hiden agian
+document.querySelectorAll(".close").forEach(btn => {
+    btn.onclick = () => {
+        btn.closest(".popup").style.display = "none";
+    };
+});
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const section3 = document.querySelector(".section-3");
